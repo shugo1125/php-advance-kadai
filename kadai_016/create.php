3424176d5fb4f81173044e314bdc49c3bc6cb154
shugo1125 <iamsyu25@icloud.com>
2024-10-06 13:56:10 +0900

    php_book_appに変更

--- kadai_016/create.php
+++ kadai_016/create.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:dbname=php_db_app;host=localhost;charset=utf8mb4';
+$dsn = 'mysql:dbname=php_book_app;host=localhost;charset=utf8mb4';
 $user = 'root';
 // MAMPを利用しているMacユーザーの方は、''ではなく'root'を代入してください
 $password = 'root';
